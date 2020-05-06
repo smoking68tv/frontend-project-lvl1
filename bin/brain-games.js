93525d8137ea3e6219ececbd2e982cac9d4fe1d1
Alexey <100.1000@list.ru>
2020-05-06 21:48:23 +0500

    added actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { askName } from '../src/cli.mjs';
+import cli from '../src/cli.mjs';
 
 console.log('welcome to the Brain games');
-askName();
+cli.askName();
--- src/cli.mjs
+++ src/cli.mjs
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}`);
-}
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}`);
+};
 
-export {
-    askName
-}
+export default {
+  askName,
+};
